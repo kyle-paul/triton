23d3183a5e1fdc946aac421ba845183f96656ef2
Kyle Paul <kylepaul1501@gmail.com>
2024-08-03 02:43:44 +0700

    Inference with SAM ONNX Decomposed model with GRPC

--- model_repository/detection_preprocessing/1/model.py
+++ model_repository/detection_preprocessing/1/model.py
@@ -3,6 +3,7 @@ import json
 import numpy as np
 import triton_python_backend_utils as pb_utils
 from PIL import Image
+import cv2
 
 class TritonPythonModel:
 
@@ -18,16 +19,19 @@ class TritonPythonModel:
         for request in requests:
             input_image = pb_utils.get_input_tensor_by_name(request, "detection_preprocessing_input")
 
-            def image_loader(image) -> np.ndarray:
-                image = np.array(image.resize((640, 480)))
-                image = image - np.array([103.94, 123.68, 116.78]).reshape(1,1,3)
-                image = image.astype(np.float32)[None, ...]
-                return image
+            # def image_loader(image) -> np.ndarray:
+            #     image = np.array(image.resize((640, 480), Image.BILINEAR))
+            #     image = image - np.array([123.68, 103.94, 116.78]).reshape(1,1,3)
+            #     image = image.astype(np.float32)[None, ...]
+            #     return image
 
             image = Image.open(io.BytesIO(input_image.as_numpy().tobytes()))
-            image = image_loader(image)
+            # image = image_loader(image)
+            
+            blob = cv2.dnn.blobFromImage(np.array(image), 1.0, (640, 480), (123.68, 116.78, 103.94), False, False)
+            blob = np.transpose(blob, (0, 2, 3, 1))
+                
             output = pb_utils.Tensor("detection_preprocessing_output", image.astype(self.output_dtype))
-
             inference_response = pb_utils.InferenceResponse(output_tensors=[output])
             responses.append(inference_response)
 
